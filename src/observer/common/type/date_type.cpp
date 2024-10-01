951296144f9952a7030046cc1139ce4b5af20740
victor-yfzhao <victor_yfzhao@outlook.com>
2024-10-01 09:32:14 +0800

    fix: `Date` some specific inputs

--- src/observer/common/type/date_type.cpp
+++ src/observer/common/type/date_type.cpp
@@ -5,42 +5,59 @@
 
 int DateType::compare(const Value &left, const Value &right) const
 {
-  ASSERT(left.attr_type() == AttrType::DATES && right.attr_type() == AttrType::DATES, "invalid type");
+  ASSERT(left.attr_type() == AttrType::DATES && 
+        (right.attr_type() == AttrType::DATES || right.attr_type() == AttrType::CHARS), "invalid type");
+  if(right.attr_type() == AttrType::CHARS) {
+    Value tmp;
+    RC rc = set_value_from_str(tmp, right.get_string());
+    if(rc != RC::SUCCESS) {
+      LOG_WARN("Failed to set value from string. rc=%d", rc);
+      return INT32_MAX;
+    }
+    return common::compare_int((void *)&left.value_.int_value_, (void *)&tmp.value_.int_value_);
+  }
   return common::compare_int((void *)&left.value_.int_value_, (void *)&right.value_.int_value_);
 }
 
 RC DateType::set_value_from_str(Value &val, const string &data) const
 {
-    if (data.size() != 10 || data[4] != '-' || data[7] != '-') {
-        return RC::INVALID_ARGUMENT;
-    }
+  if (data.size() < 8 || data.size() > 10) {
+    return RC::INVALID_ARGUMENT;
+  }
 
-    int year, month, day;
-    try {
-        year = stoi(data.substr(0, 4));
-        month = stoi(data.substr(5, 2));
-        day = stoi(data.substr(8, 2));
-    } catch (const exception &e) {
-        return RC::INVALID_ARGUMENT;
-    }
+  size_t first_dash = data.find('-');
+  size_t second_dash = data.find('-', first_dash + 1);
 
-    if (month < 1 || month > 12 || day < 1 || day > 31) {
-        return RC::INVALID_ARGUMENT;
-    }
+  if (first_dash == string::npos || second_dash == string::npos) {
+    return RC::INVALID_ARGUMENT;
+  }
 
-    // Check for days in month
-    static const int days_in_month[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-    if (month == 2) {
-        bool is_leap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-        if (day > (is_leap ? 29 : 28)) {
-            return RC::INVALID_ARGUMENT;
-        }
-    } else if (day > days_in_month[month - 1]) {
-        return RC::INVALID_ARGUMENT;
-    }
+  int year, month, day;
+  try {
+    year = stoi(data.substr(0, first_dash));
+    month = stoi(data.substr(first_dash + 1, second_dash - first_dash - 1));
+    day = stoi(data.substr(second_dash + 1));
+  } catch (const exception &e) {
+    return RC::INVALID_ARGUMENT;
+  }
 
-    val.value_.int_value_ = year * 10000 + month * 100 + day;
-    return RC::SUCCESS;
+  if (month < 1 || month > 12 || day < 1 || day > 31) {
+    return RC::INVALID_ARGUMENT;
+  }
+
+  // Check for days in month
+  static const int days_in_month[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+  if (month == 2) {
+    bool is_leap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    if (day > (is_leap ? 29 : 28)) {
+      return RC::INVALID_ARGUMENT;
+    }
+  } else if (day > days_in_month[month - 1]) {
+    return RC::INVALID_ARGUMENT;
+  }
+
+  val.value_.int_value_ = year * 10000 + month * 100 + day;
+  return RC::SUCCESS;
 }
 
 
